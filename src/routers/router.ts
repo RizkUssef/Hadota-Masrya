8944e0816775c7d38a23f9fe84f17171e0b409e2
RizkUssef <rizkusgt67@gmail.com>
2026-03-11 12:09:36 +0200

    add(router):create router file to set all routes that in the app

---
+++ src/routers/router.ts
@@ -0,0 +1,17 @@
+export const ROUTES = {
+  // public
+  home:       "/",
+  about:      "/about",
+
+  // auth
+  login:      "/auth/login",
+  register:   "/auth/register",
+
+  // chat
+  chat:       "/chat",
+  chatRoom:   (id: string) => `/chat/${id}`,
+
+  // profile
+  profile:    "/profile",
+  settings:   "/profile/settings",
+} as const;
