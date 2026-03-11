905a0a8d1b0d5ca19c353a801ae0b714c00448b3
RizkUssef <rizkusgt67@gmail.com>
2026-03-11 12:03:38 +0200

    add(home):set the home page design and it's animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,167 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import Link from "next/link";
+import { ROUTES } from "@/routers/router";
 
-export default function Home() {
+const Home = () => {
+  const hadVariants = {
+    initial: { x: -200, opacity: 0 },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const otVariants = {
+    initial: { x: -200, opacity: 0 },
+    animate: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const masryVariants = {
+    initial: { y: 200, opacity: 0 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+  const footerHeloghVariants = {
+    initial: { y: 200, opacity: 0 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+  const buttonVariants = {
+    initial: { opacity: 0 },
+    animate: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  // controls so we can fire each animation sequentially
+  const hadControls = useAnimation();
+  const otControls = useAnimation();
+  const masryControls = useAnimation();
+  const footerHeloControls = useAnimation();
+  const buttonControls = useAnimation();
+
+  useEffect(() => {
+    const runSequence = async () => {
+      await hadControls.start("animate");
+      await otControls.start("animate");
+      await masryControls.start("animate");
+      await buttonControls.start("animate");
+      await footerHeloControls.start("animate");
+    };
+
+    runSequence();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex flex-col items-center justify-center h-screen  relative overflow-hidden">
+      <div className="flex flex-col">
+        <div className="flex items-center justify-center">
+          <motion.div
+            variants={hadVariants}
+            initial="initial"
+            animate={hadControls}
+            className="font-family-en-logo text-4xl font-bold text-logo-had"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Had
+          </motion.div>
+          <motion.div
+            variants={otVariants}
+            initial="initial"
+            animate={otControls}
+            className="font-family-ar-logo text-4xl font-bold text-logo-ota -scale-x-100"
+            dir="ltr"
           >
-            Read our docs
-          </a>
+            وته
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="self-end">
+          <motion.div
+            variants={masryVariants}
+            initial="initial"
+            animate={masryControls}
+            className="font-family-ar-logo text-sm font-bold text-logo-ota"
+          >
+            مصرية
+          </motion.div>
+        </div>
+      </div>
+      <div className="flex flex-col mt-10 items-center justify-center gap-6">
+        <motion.div
+          variants={buttonVariants}
+          initial="initial"
+          animate={buttonControls}
+          className="font-family-ar-logo cursor-pointer text-sm font-bold text-logo-ota"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Link href={ROUTES.register} className="buttons">
+           🪶 لتبدأ حدوتتك الخاصة
+           {/* 📜 لتبدأ حدوتتك الخاصة */}
+          </Link>
+        </motion.div>
+      </div>
+      <div className="flex absolute bottom-0 mb-10 flex-col items-center justify-center gap-4">
+        <motion.div
+          variants={footerHeloghVariants}
+          initial="initial"
+          animate={footerHeloControls}
+          className="font-family-ar-logo text-sm font-bold text-logo-ota"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          𓂓𓂧𓅱𓏏𓏏𓇋 · 𓅓𓊃𓂋𓇋𓏏
+        </motion.div>
+        <motion.div
+          variants={footerHeloghVariants}
+          initial="initial"
+          animate={footerHeloControls}
+          className="font-family-Elmessiri text-sm font-bold text-logo-ota"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          𓂀 من أرض النيل جاءت الحكاية، لتسكن بين الناس قبل أن تُكتب 𓂀
+        </motion.div>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
