5379a1a62067255f7d84e392e0bbc8ba273dcd45
RizkUssef <rizkusgt67@gmail.com>
2026-03-12 10:00:28 +0200

    add(login):create the login function and hook and request

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,15 @@
-import { register } from "@/services/api/authApi";
+import { login, register } from "@/services/api/authApi";
+import { LoginRequest } from "@/types/LoginRequest";
 import { RegisterRequest } from "@/types/RegisterRequest";
 import { useMutation } from "@tanstack/react-query";
 
 export const useRegister = () => {
   return useMutation({
     mutationFn: (registerRequest: RegisterRequest) => register(registerRequest),
+  });
+};
+export const useLogin = () => {
+  return useMutation({
+    mutationFn: (loginRequest: LoginRequest) => login(loginRequest),
   });
 };
--- src/services/api/authApi.ts
+++ src/services/api/authApi.ts
@@ -1,3 +1,4 @@
+import { LoginRequest } from "@/types/LoginRequest";
 import { RegisterRequest } from "@/types/RegisterRequest";
 import axiosInstance from "@/utils/axiosInstance";
 
@@ -7,6 +8,17 @@ export const register = async (registerRequest: RegisterRequest) => {
     return response.data;
   } catch (err: any) {
     console.error("Register error:", err.response?.data || err.message);
+    throw err;
+  }
+};
+export const login = async (loginRequest: LoginRequest) => {
+  try {
+    const response = await axiosInstance.post("/login", loginRequest);
+    // store.dispatch(setToken(response.data.token));
+    // handle token
+    return response.data;
+  } catch (err: any) {
+    console.error("login error:", err.response?.data || err.message);
     throw err;
   }
 };
